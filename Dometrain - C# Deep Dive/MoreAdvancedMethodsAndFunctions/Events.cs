80c21db8665db15c22057c98e42e4b0b99decb0b
Greg Berlin <greg.berlin@mecca.com.au>
2024-09-20 20:46:24 +1000

    Events. A bit confused in this lesson, must review and practice this more.

--- Dometrain - C# Deep Dive/MoreAdvancedMethodsAndFunctions/Events.cs
+++ Dometrain - C# Deep Dive/MoreAdvancedMethodsAndFunctions/Events.cs
@@ -8,11 +8,12 @@
         // notify other components in our system about changes
         // they care about.
 
-        // in the most common scenarios, events use
-        // an EventHandler delegate, which accepts
-        // a type called EventArgs. The delegate
-        // signature is:
-        // public delegate void EventHandler<TEventArgs>(object sender, TEventArgs e)
+        // Event handlers will get executed in the same order in
+        //   which they are chained on to the event.
+
+        // in the most common scenarios, events use an EventHandler delegate, which accepts
+        // a type called EventArgs. The delegate signature is:
+        //public delegate void EventHandler<TEventArgs>(object sender, TEventArgs e)
 
         // the "sender" parameter is the object that raised the event
         // the "e" parameter is an instance of the EventArgs class
@@ -26,6 +27,7 @@
         source.RaiseEvent("Hello, world!");
 
         // we can remove the event with -=
+        // This is safe to run on a source that doesn't have any events assigned on it
         source.SourceChanged -= Source_SourceChanged;
 
         // but why the += and -= syntax? why aren't
@@ -34,6 +36,17 @@
         source.SourceChanged += Source_SourceChanged1;
         source.SourceChanged += Source_SourceChanged2;
         source.RaiseEvent("Hello, world! From Nick");
+
+        // What gets output here is:
+
+        //  Sender: EventSource
+        //  Message: Hello, world!
+        //  This is the first handler!
+        //  Sender: EventSource
+        //  Message: Hello, world! From Nick
+        //  This is the second handler!
+        //  Sender: EventSource
+        //  Message: Hello, world! From Nick
 
         void Source_SourceChanged(object? sender, MessageEventArgs e)
         {
@@ -60,6 +73,8 @@
         // of the event that we want them to call our method
         // (the handler) when something happens.
 
+        #region Beware of Memory Leaks
+
         // NOTE: complex systems with events can lead to memory leaks
         // if you don't unsubscribe from events when you're done with them
         // because the event source will keep a reference to the handler
@@ -67,10 +82,14 @@
         // and the garbage collector won't be able to clean them up.
         // If your event subscriptions live for the lifetime of the
         // application, then this isn't really a problem.
+
+        #endregion
     }
 
-    // let's create our own EventArgs class for sending a message!
-    public class MessageEventArgs : EventArgs
+    #region Creating our own EventArgs
+
+     // let's create our own EventArgs class for sending a message!
+   public class MessageEventArgs : EventArgs
     {
         public MessageEventArgs(string message)
         {
@@ -79,6 +98,8 @@
 
         public string Message { get; }
     }
+
+    #endregion
 
     public class EventSource
     {
@@ -89,29 +110,29 @@
 
         public void RaiseEvent(string message)
         {
+            #region Checking for null - old ways..
+
             SourceChanged.Invoke(this, new MessageEventArgs(message));
 
-            /*
             // but we need to check for null!
             if (SourceChanged != null)
             {
                 SourceChanged(this, new MessageEventArgs(message));
+                // This is the same as SourceChanged.Invoke(..)
+                // Slight possibility of a race condition, if the source changes between checking and invoking
             }
-            */
 
-            /*
-            // arguably there could be a race condition here so:
+            // arguably there could be a race condition here (above) so:
             var handler = SourceChanged;
             if (handler != null)
             {
                 handler(this, new MessageEventArgs(message));
             }
-            */
 
-            /*
+            #endregion  
+
             // but that can be greatly simplified to the following:
             SourceChanged?.Invoke(this, new MessageEventArgs(message));
-            */
         }
     }
 }
