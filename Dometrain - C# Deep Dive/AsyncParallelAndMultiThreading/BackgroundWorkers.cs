4d9beb94edd2a7d71a28bda8088bcbab1782ff31
Greg Berlin <greg.berlin@mecca.com.au>
2024-09-26 17:11:22 +1000

    Background workers and threads

--- Dometrain - C# Deep Dive/AsyncParallelAndMultiThreading/BackgroundWorkers.cs
+++ Dometrain - C# Deep Dive/AsyncParallelAndMultiThreading/BackgroundWorkers.cs
@@ -7,11 +7,13 @@ public class BackgroundWorkers
         // we can use a BackgroundWorker to run a method in the background
         // and historically this was used a lot in WinForms applications
 
+        #region Basic Example
+
         // here's how we'd create a new BackgroundWorker
         BackgroundWorker worker1 = new BackgroundWorker();
 
-        // we can then subscribe to the DoWork event
-        worker1.DoWork += (sender, e) =>
+        // we can then subscribe to the DoWork event (passing in an anonymous (or non) method)
+        worker1.DoWork += (object sender, DoWorkEventArgs e) =>
         {
             // all of this code is what's run in the background
             while (true)
@@ -20,11 +22,18 @@ public class BackgroundWorkers
                 Thread.Sleep(1000);
             }
 
+            // The following will never be called
             Console.WriteLine("Worker 1: DoWork has completed.");
         };
         worker1.RunWorkerAsync();
 
-        /*
+        // Needs the other executing code at the end of the RunExample method,
+        //  otherwise it'll just terminate immediately
+
+        #endregion
+
+        #region Passing Parameters
+
         // like with threads, we can pass parameters into the background worker
         // when we start it
         BackgroundWorker worker2 = new BackgroundWorker();
@@ -40,25 +49,45 @@ public class BackgroundWorkers
             Console.WriteLine("Worker 2: DoWork has completed.");
         };
         worker2.RunWorkerAsync(5);
-        */
+        
+        #endregion
 
-        /*
+        #region Handling Completion of Events
+
         // we can also subscribe to the RunWorkerCompleted event
         worker2.RunWorkerCompleted += (sender, e) =>
         {
             Console.WriteLine("Background Worker 2 has completed.");
         };
-        */
 
-        /*
-        // we can cancel the background worker too! let's modify
-        // worker2 to cancel worker1 when it finishes.
+        #endregion
+
+        #region Cancellation of Events
+
+        // we can cancel the background worker too! let's create a new
+        //  cancellable worker (worker3), and modify
+        // worker2 to cancel worker3 when it finishes.
+
+        BackgroundWorker worker3 = new BackgroundWorker();
+        worker3.WorkerSupportsCancellation = true;
+
+        worker3.DoWork += (sender, e) =>    
+        {
+            while (!worker3.CancellationPending)
+            {
+                Console.WriteLine("Worker 3: Working in the background...");
+                Thread.Sleep(1000); // Note cancellation will not stop this sleep execution
+            }
+        }
+
+        // Now we can make worker2 cancel worker3 when it finishes
         worker2.RunWorkerCompleted += (sender, e) =>
         {
             Console.WriteLine("Background Worker 2 has completed.");
-            worker1.CancelAsync();
+            worker3.CancelAsync();
         };
-        */
+
+        #endregion
 
         Console.WriteLine("Press enter to exit.");
         Console.ReadLine();
--- Dometrain - C# Deep Dive/AsyncParallelAndMultiThreading/Threads.cs
+++ Dometrain - C# Deep Dive/AsyncParallelAndMultiThreading/Threads.cs
@@ -3,32 +3,48 @@
     public void RunExample()
     {
         // Thread objects in C# allow us to create and manage threads.
-
+        // Pass in anonymous delegate
         Thread thread = new Thread(() =>
         {
             // do stuff
         });
         thread.Start();
 
+        #region Pass Parameters into Threads
 
-        // we can also pass parameters to the thread
+        // We can also pass parameters to the thread
+        // Good practice to pass parameters in rather than having threads access external variables
+
+        // Following is a custom type declared at end of the file
         ThreadContext thread1Context = new(
             Name: "Thread 1",
             Message: "Hello from thread 1!");
 
+        // Still an anonymous delegate, but this one takes in 1 parameter (o),
+        //  which is passed in when we start the thread
         Thread thread1 = new Thread(new ParameterizedThreadStart(o =>
         {
+            // Need to cast as the object received by this function is a generic object
             ThreadContext context = (ThreadContext)o;
 
             Thread.CurrentThread.Name = context.Name;
-            Console.WriteLine($"{Thread.CurrentThread.Name}: {context.Message}");
+            Console.WriteLine($"{Thread.CurrentThread.Name}: {context.Message}"); // Result:  Thread 1 : Hello from thread 1!
         }));
+
+        // Start the thread and pass in the threadContext object
         thread1.Start(thread1Context);
 
+        #endregion
+
+        #region Running in Background
+
         // threads can be useful for running work in the background for us
+
+        // Thread 2 example: runs in foreground, and continues forever
         ThreadContext thread2Context = new(
             Name: "Thread 2",
             Message: "Hello from thread 2!");
+
         Thread thread2 = new Thread(new ParameterizedThreadStart(o =>
         {
             ThreadContext context = (ThreadContext)o;
@@ -42,6 +58,10 @@
             }
         }));
         thread2.Start(thread2Context);
+
+
+
+        // Thread 3 example: Background Thread
 
         // we can also set a thread to be a background thread
         // which will automatically stop when the main thread stops
@@ -60,11 +80,15 @@
                 Thread.Sleep(1000);
             }
         }));
-        thread3.IsBackground = true;
+        thread3.IsBackground = true; // This is how we mark a thread as a background thread
         thread3.Start(thread3Context);
 
         Console.WriteLine("Press enter to stop Thread3.");
-        Console.ReadLine();
+        Console.ReadLine(); // program will exit when a button is pressed, as the thread is marked as background
+
+        // Can also cancel tokens, see Cancelation Tokens
+
+        #endregion
     }
 
     record ThreadContext(
