298d2ca1879486a2e8b9db9f4f2ad7e28be53adb
Greg Berlin <greg.berlin@mecca.com.au>
2024-10-04 16:47:00 +1000

    Tasks

--- Dometrain - C# Deep Dive/AsyncParallelAndMultiThreading/Tasks.cs
+++ Dometrain - C# Deep Dive/AsyncParallelAndMultiThreading/Tasks.cs
@@ -6,6 +6,8 @@
         // Using Task objects, we can get more control over
         // how we'd like our asynchronous operations to be executed.
 
+        #region Basic Task Creation
+
         // Let's run some tasks and see which threads they are executed on:
         Console.WriteLine($"Main Thread Id: {Thread.CurrentThread.ManagedThreadId}");
 
@@ -27,22 +29,23 @@
                 Thread.Sleep(1000);
             }
         });
+        
+        // something is weird with this output! why did task3 not perform its iterations as we'd expect?
+        //  Because tasks run as background threads, and the main thread completed before the for loop completed.
 
-        /*
-        // something is weird with this output! why did task3
-        // not perform its iterations as we'd expect?
-        */
+        #endregion
 
-        /*
-        // we should wait for the tasks to complete
-        // before allowing continued execution:
+        #region Task.WaitAll and Task.WaitAny
+        
+        // To get around this, we should wait for the tasks to complete before allowing continued execution:
         Task.WaitAll(task1, task2, task3);
-        Console.WriteLine("Tasks 1, 2, and 3 have completed.");
-        */
+        Console.WriteLine("Tasks 1, 2, and 3 have completed.");        
 
-        /*
-        // we can even wait for all three tasks to complete
-        // before we start a 4th task, which we will also wait on
+        // Another option is Task.WaitAny(task1, task2, task3), which waits for any of the tasks to complete 
+        //  and returns the completed task.
+        
+        // we can even wait for all three tasks to complete before we start a 4th task, 
+        //  which we will also wait on
         Task task4 = Task.Run(() =>
         {
             for (int i = 0; i < 5; i++)
@@ -51,25 +54,39 @@
                 Thread.Sleep(500);
             }
         });
+
+        // Can use a wait operation directly on the task
         task4.Wait();
         Console.WriteLine("Task 4 has completed.");
-        */
 
-        /*
-        // we can also use the "builder pattern" to chain things together
-        // on task objects:
+        #endregion
+                
+
+        #region Builder Pattern
+
+        // we can also use the "builder pattern" to chain things together on task objects.
+        // Here, we chain our tasks together using the ContinueWith method, which has access 
+        //  to the previous task's result (via the prevTask property).
         Task task5 = Task.Run(() =>
         {
             Console.WriteLine($"Task 5 Thread Id: {Thread.CurrentThread.ManagedThreadId}");
         }).ContinueWith((prevTask) =>
         {
+            // This will run on the same thread as the previous task
             Console.WriteLine($"Task 5 Continuation Thread Id: {Thread.CurrentThread.ManagedThreadId}");
         });
         task5.Wait();
-        */
+        
+        #endregion
 
-        /*
-        }).ContinueWith((prevTask) =>
+        #region Builder Pattern with Exception Handling
+
+        // We can also handle exceptions that occur during the execution of our tasks, and handle them properly
+        //  like a try catch.
+        // In this example, we will throw an exception in the first task, and catch it in the continuation task.
+        // The continuation task will catch the exception and print it out, but only if the first task threw an exception.
+        //  (TaskContinuationOptions.OnlyOnFaulted) will ensure that the continuation task only runs if the first task throws an exception).
+        Task task6 = Task.Run(() =>
         {
             Console.WriteLine($"Task 5 Continuation Thread Id: {Thread.CurrentThread.ManagedThreadId}");
             throw new Exception("We intended to do this!");
@@ -78,11 +95,13 @@
             Console.WriteLine($"Task 5 Continuation 2 Thread Id: {Thread.CurrentThread.ManagedThreadId}");
             Console.WriteLine($"{prevTask.Exception.GetType().Name}: {prevTask.Exception.Message}");
         }, TaskContinuationOptions.OnlyOnFaulted);
-        */
+        
+        #endregion
 
-        /*
-        // aggregate exceptions are a way to handle multiple exceptions
-        // that can occur when working with tasks
+        #region Aggregage Exceptions
+
+        // Aggregate exceptions are a way to handle multiple exceptions that can occur when working with tasks
+        // They allow you to pass in multiple innner exceptions.
         AggregateException aggregateException = new(
             "This is the aggregage exception message.",
             new InvalidOperationException("This is the first inner exception."),
@@ -100,6 +119,7 @@
                 Console.WriteLine($"\t{innerEx.GetType().Name}: {innerEx.Message}");
             }
         }
-        */
+        
+        #endregion
     }
 }
