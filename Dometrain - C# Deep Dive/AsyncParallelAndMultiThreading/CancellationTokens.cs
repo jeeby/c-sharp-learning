893fc37e986657c5b36b705f8493942556c5740e
Greg Berlin <greg.berlin@mecca.com.au>
2024-10-09 18:48:09 +1100

    Cancellation Tokens

--- Dometrain - C# Deep Dive/AsyncParallelAndMultiThreading/CancellationTokens.cs
+++ Dometrain - C# Deep Dive/AsyncParallelAndMultiThreading/CancellationTokens.cs
@@ -5,23 +5,32 @@
         // we can use cancellation tokens with our async/await code
         // to cancel tasks that are running:
 
+        // It's recommended to always pass in a CancellationToken to all async/await methods
+
+        #region Using Cacenllatoin Tokens
+
         // we can get a token from a CancellationTokenSource:
         CancellationTokenSource cts = new CancellationTokenSource();
         var cancellationToken = cts.Token;
 
+        // Pass the token in to the task
         async Task LoopUntilCancelledAsync(
             CancellationToken cancellationToken)
         {
             await Task.Yield();
             Console.WriteLine("Looping until cancelled...");
 
+            // Requesting cancellation is the only way this loop will end
             while (!cancellationToken.IsCancellationRequested)
             {
                 Console.WriteLine("Waiting...");
 
+                // Pass cancellationToken to Tasks so they can be canceled
                 await Task.Delay(3000, cancellationToken);
-
-                /*
+                // This wil throw an exception when the cancellatoinToken is canceled   
+                // Therefore, need to handle the exception when the cancellation token is canceled             
+                //  if you need to handle it gracefully. Otherwise, let it bubble up and handle it somewhere else
+                
                 try
                 {
                     await Task.Delay(3000, cancellationToken);
@@ -30,8 +39,12 @@
                 {
                     break;
                 }
-                */
+                
             }
+
+            // Alternative way to handle cancellation (throws an exception):
+            // Not as good as using exceptions for control flow is not recommended
+            cancellationToken.ThrowIfCancellationRequested();
 
             Console.WriteLine("Cancelled.");
         }
@@ -40,11 +53,17 @@
         Task loopTask = LoopUntilCancelledAsync(cancellationToken);
 
         Console.ReadLine();
+
+        // When a value is entered, we cancel the loop.
         cts.Cancel();
 
+        // Make sure the loopTask is finally exited before we leave the program
         await loopTask;
 
-        /*
+        #endregion
+
+        #region Chaining Cancellation Tokens
+
         // we can chain cancellation tokens together:
         CancellationTokenSource cts2 = new CancellationTokenSource();
         var cancellationToken2 = cts2.Token;
@@ -59,6 +78,7 @@
         cts2.Cancel();
 
         await loopTask;
-        */
+
+        #endregion
     }
 }
