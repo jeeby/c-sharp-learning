9f2b6cdbb9d3fe48838695546190fed90f313f02
Greg Berlin <greg.berlin@mecca.com.au>
2024-10-29 17:33:22 +1100

    DB infra code - unversioned files (oops)

---
+++ Dometrain - REST APIs Zero to Hero/Application/Movies.Application/Database/DbInitializer.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+
+namespace Movies.Application.Database;
+
+public class DbInitializer
+{
+    private readonly IDbConnectionFactory _dbConnectionFactory;
+
+    public DbInitializer(IDbConnectionFactory dbConnectionFactory)
+    {
+        _dbConnectionFactory = dbConnectionFactory;
+    }
+
+    public async Task InitializeAsync()
+    {
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+
+        // Create the database
+        await connection.ExecuteAsync($"""
+                                       create table if not exists movies (
+                                           id UUID primary key,
+                                           slug TEXT not null,
+                                           title TEXT not null,
+                                           yearofrelease integer not null);
+                                       """);
+        
+        // Create index on slug 
+        await connection.ExecuteAsync($"""
+                                       create unique index concurrently if not exists movies_slug_idx
+                                       on movies
+                                       using btree(slug);                                      
+                                       """);
+
+    }
+}
